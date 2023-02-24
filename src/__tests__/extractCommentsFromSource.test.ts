1b3f7a23d88d078ea9bb2a340dc5c75c7e4c56aa
Zachary Jones <zachary@jones.cx>
2023-02-24 16:08:12 -0500

    feat: Add merging of contiguous CommentLine nodes

--- src/__tests__/extractCommentsFromSource.test.ts
+++ src/__tests__/extractCommentsFromSource.test.ts
@@ -1,20 +1,79 @@
 import { stripIndent } from "common-tags"
 import { extractCommentsFromSource } from "../extractCommentsFromSource"
 
-test("basic single line comments", async () => {
-  const exampleCode = stripIndent`
-    import foo from 'bar';
+describe("CommentBlock", () => {
+  // CommentBlock is pretty easy since Babel already takes care of multiple lines.
+  test("basic comments", async () => {
+    const exampleCode = stripIndent`
+      import foo from 'bar';
 
-    function myFunction() {
-      // TODO This needs some work
-      return foo();
-    }
-  `
+      /**
+       * A multi-line CommentBlock here.
+       */
+      function myFunction() {
+        /* This is a single line one. */
+        return foo();
+      }
+    `
 
-  expect(await extractCommentsFromSource(exampleCode)).toEqual([
-    {
-      value: " TODO This needs some work",
-      loc: { start: { column: 2, line: 4, index: 50 }, end: { column: 30, line: 4, index: 78 } },
-    },
-  ])
+    expect(await extractCommentsFromSource(exampleCode)).toEqual([
+      {
+        value: "*\n * A multi-line CommentBlock here.\n ",
+        loc: { start: { column: 0, line: 3, index: 24 }, end: { column: 3, line: 5, index: 66 } },
+      },
+      {
+        value: " This is a single line one. ",
+        loc: { start: { column: 2, line: 7, index: 93 }, end: { column: 34, line: 7, index: 125 } },
+      },
+    ])
+  })
+})
+
+describe("CommentLine", () => {
+  test("basic single line comments", async () => {
+    const exampleCode = stripIndent`
+      import foo from 'bar';
+
+      function myFunction() {
+        // TODO This needs some work
+        return foo();
+      }
+    `
+
+    expect(await extractCommentsFromSource(exampleCode)).toEqual([
+      {
+        value: " TODO This needs some work",
+        loc: { start: { column: 2, line: 4, index: 50 }, end: { column: 30, line: 4, index: 78 } },
+      },
+    ])
+  })
+
+  test("multi-line merging", async () => {
+    const exampleCode = stripIndent`
+      import foo from 'bar';
+
+      function myFunction() {
+        // TODO This should be
+        // multiple lines
+        // long
+
+        // This is another comment but
+        // not a part of the one above.
+      }
+    `
+
+    expect(await extractCommentsFromSource(exampleCode)).toEqual([
+      {
+        value: " TODO This should be\n multiple lines\n long",
+        loc: { start: { column: 2, line: 4, index: 50 }, end: { column: 9, line: 6, index: 102 } },
+      },
+      {
+        value: " This is another comment but\n not a part of the one above.",
+        loc: {
+          start: { column: 2, line: 8, index: 106 },
+          end: { column: 33, line: 9, index: 170 },
+        },
+      },
+    ])
+  })
 })
--- src/extractCommentsFromSource.ts
+++ src/extractCommentsFromSource.ts
@@ -22,6 +22,31 @@ export const extractCommentsFromSource = async (sourceText: string): Promise<Com
   }
 
   return ast.comments.reduce((acc, comment) => {
+    // If the comment is a CommentBlock, we don't need to anything special for it.
+    if (comment.type === "CommentBlock") {
+      return [
+        ...acc,
+        {
+          value: comment.value,
+          loc: { start: { ...comment.loc!.start }, end: { ...comment.loc!.end } },
+        } as CommentNode,
+      ]
+    }
+
+    // If the comment is a CommentLine, we need to merge it with the previous comment
+    // if it ended on the line before this one.
+    const previous: CommentNode | undefined = acc[acc.length - 1]
+
+    if (previous && previous.loc!.end.line === comment.loc!.start.line - 1) {
+      return [
+        ...acc.slice(0, -1),
+        {
+          value: `${previous.value}\n${comment.value}`,
+          loc: { start: { ...previous.loc!.start }, end: { ...comment.loc!.end } },
+        } as CommentNode,
+      ]
+    }
+
     return [
       ...acc,
       {
